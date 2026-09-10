19f8b118b8203473122b8c4983d424657fb42eda
whippop <166311784+whippop@users.noreply.github.com>
2026-09-10 12:30:38 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -44,5 +44,6 @@ if ingredients_list:
 
 #New section to display smoothiefroot nutrition information 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response.json())
+smoothiefroot_response = requests.get("(https://my.smoothiefroot.com/api/fruit/watermelon)")  
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot)response,json(), use_container_width=True)
